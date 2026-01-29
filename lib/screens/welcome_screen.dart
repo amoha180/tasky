f226b5433003b1e8246e7df4ab91850813c06f00
Abdelrhman Ahmed <rahmany483@gmail.com>
2026-01-29 09:45:50 +0200

    Add welcome screen and welcom screen assets

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff181818),
+      appBar: AppBar(
+        backgroundColor: Color(0xff181818),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset("Assets/Images/logo.svg", height: 42, width: 42),
+            SizedBox(width: 16),
+            Text("Tasky", style: TextStyle(color: Colors.white, fontSize: 28)),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Welcome To Tasky ",
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                SvgPicture.asset(
+                  "Assets/Images/waving-hand.svg",
+                  height: 28,
+                  width: 28,
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text(
+              "Your productivity journey starts here.",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+            SizedBox(height: 24),
+            SvgPicture.asset(
+              "Assets/Images/Work-in-progress.svg",
+              width: 215,
+              height: 204,
+            ),
+            SizedBox(height: 28),
+
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(345, 40),
+                foregroundColor: Color(0xffFFFCFC),
+                backgroundColor: Color(0xff15B86C),
+              ),
+              child: Text(
+                "Let’s Get Started",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
